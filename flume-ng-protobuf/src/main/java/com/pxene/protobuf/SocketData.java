59ac4f0ef6014c1d6697c683b5f0798222fc7c2b
huyang <huyang1532@gmail.com>
2014-12-06 18:00:29 +0800

    delete socket client inputstream
    Signed-off-by: huyang <huyang1532@gmail.com>

--- flume-ng-protobuf/src/main/java/com/pxene/protobuf/SocketData.java
+++ flume-ng-protobuf/src/main/java/com/pxene/protobuf/SocketData.java
@@ -3,10 +3,8 @@ package com.pxene.protobuf;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.List;
 
 import org.apache.commons.io.HexDump;
@@ -22,18 +20,14 @@ public class SocketData {
     public static void main(String[] args) {
 
         try {
-            @SuppressWarnings("resource")
-			Socket socket = new Socket("10.0.2.11", 5140);
-
+        	
+            Socket socket = new Socket("10.0.2.11", 5140);
             TanxBidding.BidRequest.Builder builder = TanxBidding.BidRequest.newBuilder();
             TanxBidding.BidRequest.AdzInfo.Builder AdzInfoOrBuilder = TanxBidding.BidRequest.AdzInfo.newBuilder();
             TanxBidding.BidRequest.UserAttribute.Builder userBuilder = TanxBidding.BidRequest.UserAttribute.newBuilder();
             TanxBidding.BidRequest.ContentCategory.Builder contentBuilder = TanxBidding.BidRequest.ContentCategory.newBuilder();
-
             builder.setBid("0a67f524000054813cfc6d8e0015ad64");
-            
             builder.setVersion(3);
-
             AdzInfoOrBuilder.setId(1);
             AdzInfoOrBuilder.setPid("mm_45015339_4154953_22394227");
             userBuilder.setId(1);
@@ -46,42 +40,22 @@ public class SocketData {
             builder.addUserAttribute(userBuilder.build());
             builder.addContentCategories(contentBuilder.build());
             TanxBidding.BidRequest request = builder.build();
-//            byte[] result = request.toByteArray();
             TanxBidding.BidRequest req = TanxBidding.BidRequest.parseFrom(request.toByteArray());
             System.out.println("bid is " + req.getBid());
             List<AdzInfo> ad = req.getAdzinfoList();
             System.out.println("pid is " + req.getAdzinfoList());
             HexDump.dump(request.toByteArray(), 0, System.out, 0);
-            InputStream in = socket.getInputStream();
             OutputStream out = socket.getOutputStream();
 	        out.write(request.toByteArray());
-	        int totalBytesRcvd = 0;
-	        int bytesRcvd = 0;
-	        while (totalBytesRcvd < request.toByteArray().length) {
-	            if ((bytesRcvd = in.read(request.toByteArray(), totalBytesRcvd, request.toByteArray().length - totalBytesRcvd)) == -1) {
-	                throw new SocketException("与服务器的连接已关闭");
-	            }
-	            totalBytesRcvd += bytesRcvd;
-	        }
-	        socket.close();
-	        System.exit(0);
-//            File file = new File("D:\\git\\flume-ng-protobuf\\src\\main\\resources\\tess.txt");
-//            @SuppressWarnings("resource")
-//			FileInputStream inputStream = new FileInputStream(file);
-//            byte[] result = new byte[inputStream.available()];
-//            inputStream.read(result);
-//            InputStream in = socket.getInputStream();
-//            OutputStream out = socket.getOutputStream();
-//            out.write(result);
-//            int totalBytesRcvd = 0;
-//            int bytesRcvd = 0;
-//            while (totalBytesRcvd < result.length) {
-//                if ((bytesRcvd = in.read(result, totalBytesRcvd, result.length - totalBytesRcvd)) == -1) {
-//                    throw new SocketException("与服务器的连接已关闭");
-//                }
-//                totalBytesRcvd += bytesRcvd;
-//            }
-//            socket.close();
+            File file = new File("D:\\git\\flume-ng-protobuf\\src\\main\\resources\\tess.txt");
+            @SuppressWarnings("resource")
+			FileInputStream inputStream = new FileInputStream(file);
+            byte[] result = new byte[inputStream.available()];
+            
+            inputStream.read(result);
+            OutputStream out1 = socket.getOutputStream();
+            out1.write(result);
+            socket.close();
 
         } catch (IOException e) {
             e.printStackTrace();

b5ba9118844630d986ddfb2d59711ad07e6937ea
huyang <huyang1532@gmail.com>
2014-12-12 18:20:30 +0800

    update

--- flume-ng-protobuf/src/main/java/com/pxene/protobuf/SocketData.java
+++ flume-ng-protobuf/src/main/java/com/pxene/protobuf/SocketData.java
@@ -11,15 +11,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.collections.list.UnmodifiableList;
 import org.apache.commons.io.HexDump;
 import org.apache.log4j.Logger;
 
 import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor.JavaType;
+import com.google.protobuf.Message;
+import com.google.protobuf.ProtocolStringList;
 import com.google.protobuf.UnmodifiableLazyStringList;
 import com.pxene.protobuf.TanxBidding.BidRequest;
 import com.pxene.protobuf.TanxBidding.BidRequest.AdzInfo;
+import com.pxene.protobuf.TanxBidding.BidRequest.Mobile;
+import com.pxene.protobuf.TanxBidding.BidRequest.PrivateInfo;
+import com.pxene.protobuf.TanxBidding.BidRequest.UserAttribute;
 
 /**
  * Created by root
@@ -28,7 +34,9 @@ import com.pxene.protobuf.TanxBidding.BidRequest.AdzInfo;
 public class SocketData {
 
 	public static final Logger logger = Logger.getLogger(SocketData.class);
-    public static void main(String[] args) {
+	
+    @SuppressWarnings("unchecked")
+	public static void main(String[] args) {
 
         try {
         	
@@ -63,45 +71,147 @@ public class SocketData {
 			FileInputStream inputStream = new FileInputStream(file);
             byte[] result = new byte[inputStream.available()];
             inputStream.read(result);
+//            Descriptor des = BidRequest.getDescriptor();
+//            List<FieldDescriptor> fieldLists = des.getFields();
+//            for (FieldDescriptor fieldList : fieldLists) {
+//				if (fieldList.getJavaType() != JavaType.MESSAGE) {
+//					logger.info(fieldList.getName());
+//				}
+//			}
+            
+            
             TanxBidding.BidRequest req = TanxBidding.BidRequest.parseFrom(result);
-            List<Map<String, String>> list = new ArrayList<Map<String,String>>();
-            Map<FieldDescriptor, Object> fields = req.getAllFields();
-            Set<FieldDescriptor> fieldDescriptors = fields.keySet();
-            for (FieldDescriptor fieldDescriptor : fieldDescriptors) {
-            	Map<String, String> map = new HashMap<String, String>();
-//            	fieldDescriptor.getJavaType() == JavaType.
-            	if (fieldDescriptor.getJavaType() == JavaType.INT) {
-            		if (fields.get(fieldDescriptor) instanceof Integer) {
-            			map.put(fieldDescriptor.getName(), String.valueOf(fields.get(fieldDescriptor)));
-					} else {
-						logger.info("is JavaType.INT not Integer");
-					}
-					list.add(map);
-				}else if (fieldDescriptor.getJavaType() == JavaType.STRING) {
-					if (fields.get(fieldDescriptor) instanceof UnmodifiableLazyStringList) {
-						@SuppressWarnings("unchecked")
-						List<String> strings = (List<String>) fields.get(fieldDescriptor);
-						StringBuilder fieldStringBuilder = new StringBuilder();
-						for (String string : strings) {
-							fieldStringBuilder.append(string);
-							fieldStringBuilder.append(new Character((char) 0x01));
-						}
-						map.put(fieldDescriptor.getName(), fieldStringBuilder.toString());
-					} else {
-						map.put(fieldDescriptor.getName(), (String)fields.get(fieldDescriptor));
-					}
-					list.add(map);
-				}else if (fieldDescriptor.getJavaType() == JavaType.MESSAGE) {
-//					logger.info(fieldDescriptor.toProto().getRepeatedFieldCount(fieldDescriptor));
-//					Object subClass = fields.get(fieldDescriptor);
-//					if (subClass instanceof AdzInfo) {
-//						
-//						AdzInfo adzInfo = (AdzInfo) subClass;
-//						adzInfoFields = adzInfo.getAllFields();
-						
-					}
-					
+            StringBuilder sBuilder = new StringBuilder();
+            String NULL = "NULL";
+            sBuilder.append(req.getVersion()).append("|");
+            sBuilder.append(req.getBid()).append("|");
+            
+            if (req.hasIsTest()) {
+            	sBuilder.append(req.getIsTest()).append("|");
+			} else {
+				sBuilder.append(NULL).append("|");
+			}
+            
+            if (req.hasIsPing()) {
+            	sBuilder.append(req.getIsPing()).append("|");
+			} else {
+				sBuilder.append(NULL).append("|");
+			}
+            
+            if (req.hasTid()) {
+            	sBuilder.append(req.getTid()).append("|");
+			} else {
+				sBuilder.append(NULL).append("|");
+			}
+            
+            if (req.hasIp()) {
+				sBuilder.append(req.getIp()).append("|");
+			} else {
+				sBuilder.append(NULL).append("|");
+			}
+            
+            if (req.hasUserAgent()) {
+				
+            	sBuilder.append(req.getUserAgent()).append("|");
+			} else {
+				sBuilder.append(NULL).append("|");
+			}
+            
+            if (req.hasTimezoneOffset()) {
+            	
+            	sBuilder.append(req.getTimezoneOffset()).append("|");
+            } else {
+            	sBuilder.append(NULL).append("|");
+            }
+            
+            if (req.hasTimezoneOffset()) {
+            	
+            	sBuilder.append(req.getTimezoneOffset()).append("|");
+            } else {
+            	sBuilder.append(NULL).append("|");
+            }
+            
+            List<Integer> userVertical = req.getUserVerticalList();
+            if (userVertical.isEmpty()) {
+            	sBuilder.append(NULL).append("|");
+			} else {
+				StringBuilder userVerticalStringBuilder = new StringBuilder();
+				for (Integer integer : userVertical) {
+					userVerticalStringBuilder.append(integer).append(new Character((char)0x01));
 				}
+				sBuilder.append(userVerticalStringBuilder.toString().substring(0, 
+						userVerticalStringBuilder.toString().length()-2)).append("|");
+			}
+            
+            if (req.hasTidVersion()) {
+            	sBuilder.append(req.getTidVersion()).append("|");
+			} else {
+				sBuilder.append(NULL).append("|");
+			}
+            
+            ProtocolStringList excludedUrls = req.getExcludedClickThroughUrlList();
+            if (excludedUrls.isEmpty()) {
+            	sBuilder.append(NULL).append("|");
+			} else {
+				StringBuilder excludedUrlsBuilder = new StringBuilder();
+				for (String string : excludedUrls) {
+					excludedUrlsBuilder.append(string).append(new Character((char)0x01));
+				}
+				sBuilder.append(excludedUrlsBuilder.toString().substring(0, 
+						excludedUrlsBuilder.toString().length()-2)).append("|");
+			}
+            
+            sBuilder.append(req.getPrivateInfoCount()).append("|");
+            
+            if (req.hasHostedMatchData()) {
+				sBuilder.append(req.getHostedMatchData()).append("|");
+			
+			} else {
+				sBuilder.append(NULL).append("|");
+			}
+            
+            sBuilder.append(req.getUserAttributeCount()).append("|");
+            
+            
+            
+            
+            
+            
+//            Map<String, String> map = new HashMap<String, String>();
+//            Map<FieldDescriptor, Object> fields = req.getAllFields();
+//            Set<FieldDescriptor> fieldDescriptors = fields.keySet();
+//            for (FieldDescriptor fieldDescriptor : fieldDescriptors) {
+//            	if (fieldDescriptor.getJavaType() == JavaType.INT) {
+//            		if (fields.get(fieldDescriptor) instanceof Integer) {
+//            			map.put(fieldDescriptor.getName(), String.valueOf(fields.get(fieldDescriptor)));
+//					} else if (fields.get(fieldDescriptor) instanceof UnmodifiableList) {
+//						List<Integer> integers = (List<Integer>) fields.get(fieldDescriptor);
+//						StringBuilder integerStringBuilder = new StringBuilder();
+//						for (Integer integer : integers) {
+//							integerStringBuilder.append(String.valueOf(integer));
+//							integerStringBuilder.append(new Character((char) 0x01));
+//						}
+//					} else {
+//						logger.info("is JavaType.INT not Integer");
+//					}
+//				}else if (fieldDescriptor.getJavaType() == JavaType.STRING) {
+//					if (fields.get(fieldDescriptor) instanceof UnmodifiableLazyStringList) {
+//						
+//						List<String> strings = (List<String>) fields.get(fieldDescriptor);
+//						StringBuilder fieldStringBuilder = new StringBuilder();
+//						for (String string : strings) {
+//							fieldStringBuilder.append(string);
+//							fieldStringBuilder.append(new Character((char) 0x01));
+//						}
+//						map.put(fieldDescriptor.getName(), fieldStringBuilder.toString());
+//					} else {
+//						String fieldValue = (String)fields.get(fieldDescriptor);
+//						map.put(fieldDescriptor.getName(), fieldValue.substring(0, fieldValue.length()-2));
+//					}
+//					
+//				}
+//					
+//			}
 
 //			}
 //            OutputStream out1 = socket.getOutputStream();
@@ -112,4 +222,5 @@ public class SocketData {
             logger.error("error is " + e.toString());
         }
     }
+    
 }
